9b8bdada2e5f74c69f53c580e2eddea2e3e9c847
KaleSakshee <sakshikale2324@gmail.com>
2025-04-05 21:48:48 +0530

    Remove unnecessary files during rebase

--- main.py
+++
@@ -1,9 +0,0 @@
-from core.fixer import background_cleaner
-
-def main():
-    print("🚀 ZeroLag AI Cleaner starting...")
-    background_cleaner(interval=600, dry_run=False)  # Set dry_run=True to preview only
-    input("🌀 Agent running in background. Press Enter to stop.\n")
-
-if __name__ == "__main__":
-    main()
